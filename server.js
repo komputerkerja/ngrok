3286387628f3d66c216847f5357d72cfd6ff3850
komputerkerja <komputerkerja100@gmail.com>
2022-08-13 01:17:20 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 require('dotenv').config()
 const ngrok = require('ngrok')
 
+function parseUrl(url) {
+	let index1 = url.indexOf('//');
+	let index2 = url.indexOf('o:');
+	let newUrl = url.slice(index1+2, index2+1);
+	let port = url.slice(index2+2);
+	let str = "ssh ";
+	str += process.env.PI_USERNAME;
+	str += "@";
+	str += newUrl;
+	str += " -p ";
+	str += port;
+	return str;
+}
+
 const options = {
     proto: 'tcp',
     addr: 22,
@@ -10,7 +24,10 @@ const options = {
 const connect = async () => {
     console.log('requesting tunnel...')
     const url = await ngrok.connect(options)
-    console.log(url)
+    console.log(`url: ${url}`)
+    const prsUrl = parseUrl(url)
+    console.log(`the command we should paste into cli`)
+    console.log(prsUrl)
 }
 
 connect()
@@ -20,3 +37,4 @@ setTimeout(async () =>  {
     await ngrok.kill()
     console.log('disconnected!')
 }, 3000);
+
