fea5026cc548e540b143cc9803c83c098cd9fc89
komputerkerja <komputerkerja100@gmail.com>
2022-08-14 01:47:33 +0700

    test ngrok for raspi

--- server.js
+++ server.js
@@ -1,20 +1,6 @@
 require('dotenv').config()
 const ngrok = require('ngrok')
 
-function parseUrl(url) {
-	let index1 = url.indexOf('//');
-	let index2 = url.indexOf('o:');
-	let newUrl = url.slice(index1+2, index2+1);
-	let port = url.slice(index2+2);
-	let str = "ssh ";
-	str += process.env.PI_USERNAME;
-	str += "@";
-	str += newUrl;
-	str += " -p ";
-	str += port;
-	return str;
-}
-
 const options = {
     proto: 'tcp',
     addr: 22,
@@ -30,11 +16,11 @@ const connect = async () => {
     console.log(prsUrl)
 }
 
-connect()
-
-setTimeout(async () =>  {
+disconnect = async () =>  {
     await ngrok.disconnect()
     await ngrok.kill()
     console.log('disconnected!')
-}, 3000);
+}
 
+connect()
+setTimeout(() => disconnect(), 100000);
